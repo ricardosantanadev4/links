c3ca757a0db4690756dcedf516af2094eb57bcfb
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-05-21 21:59:59 -0300

    Projeto criado

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+import { View } from "react-native";
+
+export default function Index() {
+    return (
+        <View>
+            <Text>Hello React Native!</Text>
+            <Text>Meu app!</Text>
+        </View>
+    )
+}

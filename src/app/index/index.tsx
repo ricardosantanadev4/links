4777b8ed58bfbb86c26ab0363d51f6458f13d676
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-05-22 23:24:08 -0300

    feat: Criação do component Category.

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,3 +1,4 @@
+import { Category } from "@/components/category";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Image, TouchableOpacity, View } from "react-native";
 import { colors } from "../../styles/colors";
@@ -12,6 +13,7 @@ export default function Index() {
                     <MaterialIcons name="add" size={32} color={colors.green[300]} />
                 </TouchableOpacity>
             </View>
+            <Category />
         </View>
     )
 }

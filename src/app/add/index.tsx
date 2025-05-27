bd3a49a4aed2365cf28c9e862eec13c605a556d2
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-05-27 15:22:11 -0300

    feat: Component de botão criado.

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/button";
 import { Categories } from "@/components/categories";
 import { Input } from "@/components/input";
 import { colors } from "@/styles/colors";
@@ -24,6 +25,7 @@ export default function Add() {
             <View style={styles.form}>
                 <Input placeholder="Nome" />
                 <Input placeholder="Url" />
+                <Button title="Adicionar" />
             </View>
         </View>
     )

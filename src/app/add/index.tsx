81e7ed10d49d6536f0996e818b766c033943bdc2
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-05-28 15:58:15 -0300

    feat: Funcionalidade de acionar listar links no storage do telefone do usuário.

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/button"
 import { Categories } from "@/components/categories"
 import { Input } from "@/components/input"
+import { linksStorage } from "@/storage/link-storage"
 import { colors } from "@/styles/colors"
 import { MaterialIcons } from "@expo/vector-icons"
 import { router } from "expo-router"
@@ -9,24 +10,41 @@ import { Alert, Text, TouchableOpacity, View } from "react-native"
 import { styles } from "./styles"
 
 export default function Add() {
-    const [categoy, setCategory] = useState("")
+    
+    const [category, setCategory] = useState("")
     const [name, setName] = useState("")
     const [url, setUrl] = useState("")
 
-    function handleAdd() {
-        if (!categoy) {
-            return Alert.alert("Categoria", "Selecione uma categoria")
+    async function handleAdd() {
+        try {
+            if (!category) {
+                return Alert.alert("Categoria", "Selecione uma categoria")
+            }
+
+            if (!name.trim()) {
+                return Alert.alert("Nome", "Informe um nome")
+            }
+
+            if (!url.trim()) {
+                return Alert.alert("URL", "Informe uma URL")
+            }
+
+            await linksStorage.save({
+                id: new Date().getTime().toString(),
+                name,
+                url,
+                category
+            })
+
+            const data = await linksStorage.get()
+
+            console.log({ data })
+
+        } catch (erro) {
+            Alert.alert("Erro", "Não foi possível salvar o link")
+            console.log(erro)
         }
 
-        if (!name.trim()) {
-            return Alert.alert("Nome", "Informe um nome")
-        }
-
-        if (!url.trim()) {
-            return Alert.alert("URL", "Informe uma URL")
-        }
-
-        console.log({ categoy, name, url })
     }
 
     return (
@@ -41,11 +59,11 @@ export default function Add() {
 
             <Text style={styles.label}>Selecione uma categoria</Text>
 
-            <Categories selected={categoy} onChange={setCategory} />
+            <Categories selected={category} onChange={setCategory} />
 
             <View style={styles.form}>
                 <Input placeholder="Nome" onChangeText={setName} autoCorrect={false} />
-                <Input placeholder="URL" onChangeText={setUrl} autoCorrect={false} />
+                <Input placeholder="URL" onChangeText={setUrl} autoCorrect={false} autoCapitalize="none" />
                 <Button title="Adicionar" onPress={handleAdd} />
             </View>
         </View>
